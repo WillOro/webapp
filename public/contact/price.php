49b0f99b58b3b36412fb2e7391aaebd427beff17
William Otoniel <william.otoniel@gmail.com>
2014-10-20 19:40:58 -0600

    Update
    Site presentation

---
+++ public/contact/price.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Mutual of Omaha Mobile</title>
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="js/jqm/jquery.mobile-1.4.4.min.css">
+	<!--link rel="stylesheet" href="js/jqm/_assets/css/jqm-demos.css"-->
+	<script src="js/jqm/_assets/js/jquery.js"></script>
+	<script src="js/jqm/_assets/js/index.js"></script>
+	<script src="js/jqm/jquery.mobile-1.4.4.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="bootstrap3/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap3/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap3/css/bootstrap-theme.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap3/css/bootstrap-theme.min.css">
+	<script src="bootstrap3/js/bootstrap.js"></script>
+	<script src="bootstrap3/js/bootstrap.min.js"></script>
+<style>
+#orange
+{
+	background-color:#F3930E;
+}
+.item
+{
+	background-color:#848484;
+	color:#FFF;
+	text-align:center;
+	padding: 10px 0 10px 0;
+	border-color:#848484 #848484 #a4a4a4;
+	border-style: solid;
+}
+</style>
+</head>
+</body>
+<h2>The Benefits of Owning a Medicare Supplement Insurance Policy</h2>
+<p>With all the choices you have for your health care coverage, It's important to understand how each one works, its benefits and you share 
+of the cost. Here's a brief look at how Medicare supplement insurance differs from other coverage , Including Medicare Advantage Plans.
+</p>
+<ul>
+<li>Low Out-of-Pockets Cost</li>
+<li>Keep Your Doctors and Hospitals who accept Medicare</li>
+<li>Coverage is Guaranteed Renewable</li>
+</ul>
+<p>
+<a href="../public">back</a>
+</p>
+</body>
+</html>
---
+++ public/insurance.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Mutual of Omaha Mobile</title>
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="js/jqm/jquery.mobile-1.4.4.min.css">
+	<!--link rel="stylesheet" href="js/jqm/_assets/css/jqm-demos.css"-->
+	<script src="js/jqm/_assets/js/jquery.js"></script>
+	<script src="js/jqm/_assets/js/index.js"></script>
+	<script src="js/jqm/jquery.mobile-1.4.4.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="bootstrap3/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap3/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap3/css/bootstrap-theme.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap3/css/bootstrap-theme.min.css">
+	<script src="bootstrap3/js/bootstrap.js"></script>
+	<script src="bootstrap3/js/bootstrap.min.js"></script>
+</head>
+</body>
+<h2>Medicare Supplement Insurance Basics</h2>
+        <p>Original Medicare covers many health care services and supplies, but there are many costs it doesn't cover. A medigap policy is health insurance
+		sold by private insurance companies to fill gaps in Original Medicare coverage.</p>
+		<p>
+<a href="../public">back</a>
+</p>
+</body>
+</html>
